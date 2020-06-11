d915c5209ad3a941b6a0b0d73abac08d1facbc7b
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-11 14:45:12 -0700

    Add Navbar and Layout to the App

---
+++ src/user/Signin.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../core/Layout'; 
+
+const Signin = () => {
+    return (
+        <Layout title="Sign-in Page" description="signin to Node React Ecommerce App">
+            ...
+        </Layout>
+    )
+}
+
+export default Signin
---
+++ src/user/Signup.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../core/Layout'; 
+
+const Signup = () => {
+    return (
+        <Layout title="Sign-up Page" description="signup to Node React Ecommerce App">
+            ...
+        </Layout>
+    )
+}
+
+export default Signup
