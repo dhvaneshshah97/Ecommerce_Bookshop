994853749d973614117c67925a2063b847e4712b
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-11 15:05:25 -0700

    Store API End-Point in .env file

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Layout from '../core/Layout'; 
+import Layout from '../core/Layout';
+import { API } from '../config';
 
 const Signup = () => {
     return (
         <Layout title="Sign-up Page" description="signup to Node React Ecommerce App">
-            ...
+            {API}
         </Layout>
     )
 }
