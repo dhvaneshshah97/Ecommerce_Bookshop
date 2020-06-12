28e61b9adbd35662322e517c4dbc44cb2aba64e0
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-11 19:07:04 -0700

    Create Signin Component with full validation

--- src/auth/index.js
+++ src/auth/index.js
@@ -16,3 +16,18 @@ export const signup = async (user) => {
     return response;
 
 }
+
+export const signin = async (user) => {
+    const rawResponse = await fetch(`${API}/signin`, {
+        method: "POST",
+        headers: {
+            // API will respond with json data, so we need to accept json data
+            Accept: 'application/json',
+            // we tell the back-end that we are sending json data
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(user)
+    });
+    const response = rawResponse.json();
+    return response;
+}
--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,12 +1,86 @@
-import React from 'react'
-import Layout from '../core/Layout'; 
+import React, { useState } from 'react'
+import Layout from '../core/Layout';
+import { signin } from '../auth/index';
+import { Redirect } from 'react-router-dom';
 
 const Signin = () => {
+    const [values, setValues] = useState({
+        email: '',
+        password: '',
+        error: '',
+        loading: false,
+        redirectToHome: false,
+    });
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        setValues({ ...values, error: false, loading: true })
+        const userData = await signin({ email, password });
+        if (userData.error) {
+            setValues({ ...values, error: userData.error, loading: false });
+        } else {
+            setValues({ ...values, redirectToHome: true });
+        }
+
+    }
+
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value, error: false });
+    }
+
+    const { password, email, error, loading, redirectToHome } = values;
+
+    // signin form
+    const signinForm = () => {
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <label className="text-muted">Email</label>
+                    <input className="form-control" type="email" name="email" value={email} onChange={handleChange} />
+                </div>
+                <div className="form-group">
+                    <label className="text-muted">Password</label>
+                    <input className="form-control" type="password" name="password" value={password} onChange={handleChange} />
+                </div>
+                <button className="btn btn-outline-primary" >Submit</button>
+            </form>
+        )
+    }
+
+    // showError component
+    const showError = () => {
+        return (
+            <div className="alert alert-danger" style={{ display: error ? '' : 'none' }}>
+                {error}
+            </div>
+        );
+    }
+
+    // showSuccess component
+    const showLoading = () => {
+        return (
+            <div className="alert alert-success" style={{ display: loading ? '' : 'none' }}>
+                Loading...
+            </div>
+        );
+    }
+    const redirectUser = () => {
+        if (redirectToHome) {
+            return <Redirect to="/" />
+        }
+    }
+
+
     return (
-        <Layout title="Sign-in Page" description="signin to Node React Ecommerce App">
-            ...
+        <Layout title="Sign-in Page" description="signin to Node React Ecommerce App" className="container col-md-8 offset-md-2">
+            {showError()}
+            {signinForm()}
+            {showLoading()}
+            {redirectUser()}
         </Layout>
     )
 }
 
-export default Signin
+export default Signin;
