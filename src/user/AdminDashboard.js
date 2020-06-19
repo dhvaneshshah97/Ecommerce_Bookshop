67e7f4e3ffe728ff401ba9a18c3c6307eeda61f1
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-19 16:33:20 -0700

    Create useful links on user dashboard

--- src/user/AdminDashboard.js
+++ src/user/AdminDashboard.js
@@ -3,31 +3,31 @@ import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth/index';
 import { Link } from 'react-router-dom';
 
-const Dashboard = () => {
+const AdminDashboard = () => {
 
     // destructuring user from object derived from below method which return an object from local storage. In that whole object, user is a property with its value as object. So that's why we have destructured user like below.
     const { user: { name, email, role } } = isAuthenticated()
 
-    const userLinks = () => {
+    const adminLinks = () => {
         return (
             <div className="card">
-                <h4 className="card-header">User Links</h4>
+                <h4 className="card-header">Admin Links</h4>
                 <ul className="list-group">
                     <li className="list-group-item">
-                        <Link className="nav-link" to="/cart">My Cart</Link>
+                        <Link className="nav-link" to="/create/category">Create Category</Link>
                     </li>
                     <li className="list-group-item">
-                        <Link className="nav-link" to="/profile/update">Update Profile</Link>
+                        <Link className="nav-link" to="/create/product">Create Product</Link>
                     </li>
                 </ul>
             </div>
         );
     }
 
-    const userInfo = () => {
+    const adminInfo = () => {
         return (
             <div className="card mb-5">
-                <h3 className="card-header">User Information</h3>
+                <h3 className="card-header">Admin Information</h3>
                 <ul className="list-group">
                     <li className="list-group-item">{name}</li>
                     <li className="list-group-item">{email}</li>
@@ -37,33 +37,15 @@ const Dashboard = () => {
         );
     }
 
-    const purchaseHistory = () => {
-        return (
-            <div className="card mb-5">
-                <h3 className="card-header">Purchase History</h3>
-                <ul className="list-group">
-                    <li className="list-group-item">history</li>
-                </ul>
-            </div>
-        );
-    }
-
-
 
     return (
         <Layout title="Dashboard" description={'Welcome Back, ' + name} classNameName="container-fluid">
             <div className="row">
-                <div className="col-3">
-                    {userLinks()}
-                </div>
-                <div className="col-9">
-                    {userInfo()}
-                    {purchaseHistory()}
-                </div>
+                <div className="col-3">{adminLinks()}</div>
+                <div className="col-9">{adminInfo()}</div>
             </div>
-
         </Layout>
     )
 }
 
-export default Dashboard;
+export default AdminDashboard;
