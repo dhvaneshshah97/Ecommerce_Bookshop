387f27fa3519a1d0bcfa634229d9337f88a5400c
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-06-23 17:41:40 -0700

    Style buttons and jumbotron

--- src/core/Card.js
+++ src/core/Card.js
@@ -12,9 +12,8 @@ const Card = ({ product }) => {
                     <p>{product.description}</p>
                     <p>${product.price}</p>
                     <Link to="/">
-                        <button className="btn btn-outline-primary mt-2 mb-2">View Product</button>
+                        <button className="btn btn-outline-primary mt-2 mb-2 mr-2">View Product</button>
                     </Link>
-                    {' '}
                     <button className="btn btn-outline-warning mt-2 mb-2">Add to cart</button>
                 </div>
             </div>
