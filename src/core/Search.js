3faea356369e423ed0d86ab4b39334715107c497
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-07-03 12:21:27 -0700

    Update Search bar

--- src/core/Search.js
+++ src/core/Search.js
@@ -1,13 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import Layout from './Layout';
-import { getProducts, getCategories } from './apiCore';
+import { getProducts, getCategories, list } from './apiCore';
 import Card from './Card';
 
 const Search = () => {
 
     const [data, setData] = useState({
         categories: [],
-        category: '',
+        category: 'All',
         search: '',
         results: [],
         searched: false,
@@ -30,19 +30,34 @@ const Search = () => {
     }, []);
 
     const searchSubmit = (e) => {
-        // e.preventDefault();
-    }
-    const handleChange = () => {
-        //
+        e.preventDefault();
+        searchData();
     }
 
-    return (
-        <div className="container mb-4">
-            <form onSubmit={searchSubmit}>
+    const searchData = async () => {
+        // console.log(search, category);
+        if (search || category) {
+            const response = await list({ search: search || undefined, category: category });
+            if (response.error) {
+                console.log(response.error);
+            } else {
+                setData({ ...data, results: response, searched: true });
+            }
+        }
+    }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setData({ ...data, [name]: value, searched: false });
+    }
+
+    const searchForm = () => (
+        <div className="container mb-4 " >
+            <form onSubmit={searchSubmit} className="mx-auto" style={{width: '800px'}}>
                 <span className="input-group-text">
                     <div className="input-group">
                         <div className="input-group-prepend">
-                            <select className="btn mr-2" onChange={handleChange} name="category" style={{paddingRight: '0.5px', verticalAlign:"center"}}>
+                            <select className="btn mr-2" onChange={handleChange} name="category" style={{ paddingRight: '0.5px', verticalAlign: "center" }}>
                                 <option value="All">All Categories</option>
                                 {categories.map((c, i) => (<option key={i} value={c._id}>{c.name}</option>))}
                             </select>
@@ -50,10 +65,37 @@ const Search = () => {
                         <input type="search" className="form-control" onChange={handleChange} name="search" placeholder="Search product by name" />
                     </div>
                     <div className="btn input-group-append" style={{ border: 'none' }}>
-                        <div className="input-group-text" style={{ border: 'none' }}><i className="fas fa-lg fa-search"></i></div>
+                        <div className="input-group-text" style={{ border: 'none' }} onClick={searchSubmit}><i className="fas fa-lg fa-search"></i></div>
                     </div>
                 </span>
             </form>
+        </div>
+    );
+
+    const searchMessage = (searched, results) => {
+        if (searched && results.length > 0) {
+            return `Found ${results.length} products`;
+        } 
+        if (searched && results.length < 1) {
+            return `No products found`;
+        }
+    }
+
+    const showBooks = (results = []) => (
+        <div className="container-fluid mb-3">
+            <h3 className="mt-4 mb-4">
+                {searchMessage(searched, results)}
+            </h3>
+            <div className="row">
+                {results.map((p, i) => (<Card key={i} product={p} />))}
+            </div>
+        </div>
+    )
+
+    return (
+        <div className="row">
+            {searchForm()}
+            {showBooks(results)}
         </div>
 
     )
--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -1,4 +1,5 @@
 import { API } from '../config';
+import queryString from 'query-string';
 
 export const getProducts = async (sortBy) => {
     const rawResponse = await fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=6`, {
@@ -25,6 +26,16 @@ export const getFilteredProducts = async (skip, limit, filters = {}) => {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(data),
+    });
+    const response = rawResponse.json();
+    return response;
+}
+
+export const list = async (params) => {
+    const query = queryString.stringify(params);
+    console.log('query', query);
+    const rawResponse = await fetch(`${API}/products/search?${query}`, {
+        method: "GET",
     });
     const response = rawResponse.json();
     return response;
