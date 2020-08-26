270b7d656a462b6439215e3d2cb903c238bfa157
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-26 16:21:05 -0700

    Get user info for profile update

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,10 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth/index';
 import { Link } from 'react-router-dom';
+import { read, update, updateUserLocalStorageInfo } from './apiUser';
 
-const Profile = () => (
-    <h2>profile</h2>
-);
+const Profile = (props) => {
+    const [values, setValues] = useState({
+        name: '',
+        email: '',
+        password: '',
+        error: false,
+        success: false,
+    });
+
+    const { token } = isAuthenticated();
+    const { name, email, password, error, success } = values;
+
+    const init = async (userId) => {
+        try {
+            const data = await read(userId, token);
+            setValues({ ...values, name: data.name, email: data.email });
+
+        } catch (error) {
+            setValues({ ...values, error: true });
+        }
+    }
+
+    useEffect(() => {
+        init(props.match.params.userId);
+    }, []);
+
+    return (
+        <>
+            <Layout title="Profile" description="Update your profile here" className="container-fluid">
+                <h2 className="mb-4">Profile update</h2>
+                {JSON.stringify(values)}
+            </Layout>
+        </>
+    )
+
+}
 
 export default Profile;
