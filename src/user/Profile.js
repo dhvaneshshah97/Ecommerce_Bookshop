7eecaed34534407704dde56f2d823c4b723ed774
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-08-26 17:04:10 -0700

    make Profile update working correctly for user

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth/index';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { read, update, updateUserLocalStorageInfo } from './apiUser';
 
 const Profile = (props) => {
@@ -30,11 +30,54 @@ const Profile = (props) => {
         init(props.match.params.userId);
     }, []);
 
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setValues({ ...values, [name]: value, error: false });
+    }
+
+    const clickSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const data = await update(props.match.params.userId, token, { name, email, password });
+            updateUserLocalStorageInfo(data, () => {
+                setValues({...values, name: data.name, email: data.email, success: true});
+            })
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    const redirectUser = () => {
+        if (success) {
+            return <Redirect to="/user/dashboard" />
+        }
+    }
+
+    const profileUpdate = (name, email, password) => (
+        <form>
+            <div className="form-group">
+                <label className="text-muted">Name</label>
+                <input type="text" onChange={handleChange} className="form-control" value={name} name="name" />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Email</label>
+                <input type="email" onChange={handleChange} className="form-control" value={email} name="email" />
+            </div>
+            <div className="form-group">
+                <label className="text-muted">Password</label>
+                <input type="password" onChange={handleChange} className="form-control" value={password} name="password" />
+            </div>
+            <button onClick={clickSubmit} className="btn btn-primary">Update Profile</button>
+        </form>
+    )
+
     return (
         <>
             <Layout title="Profile" description="Update your profile here" className="container-fluid">
                 <h2 className="mb-4">Profile update</h2>
-                {JSON.stringify(values)}
+                {profileUpdate(name, email, password)}
+                {redirectUser()}
             </Layout>
         </>
     )
