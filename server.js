a3208914c64b8a8dd208fcfd8f529169421f1e8e
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-02 20:22:46 -0700

    Changes for heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 const app = express();
  
 app.use(compression());
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'public')));
  
 app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
  
 const PORT = process.env.PORT || 3000;
