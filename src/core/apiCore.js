4e44dae932efcf774cf803d8ab4de9aab9687b9f
Dhvanesh Shah <dhvaneshshah56@gmail.com>
2020-09-06 11:29:55 -0700

    Change structure of try-catch in apiUser

--- src/core/apiCore.js
+++ src/core/apiCore.js
@@ -2,119 +2,137 @@ import { API } from '../config';
 import queryString from 'query-string';
 
 export const getProducts = async (sortBy) => {
-    const rawResponse = await fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=100`, {
-        method: "GET",
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const rawResponse = await fetch(`${API}/products?sortBy=${sortBy}&order=desc&limit=100`, {
+            method: "GET",
+        });
+
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const getCategories = async () => {
-    const rawResponse = await fetch(`${API}/categories`, {
-        method: "GET",
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const rawResponse = await fetch(`${API}/categories`, {
+            method: "GET",
+        });
+
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const getFilteredProducts = async (skip, limit, filters = {}) => {
-    const data = { limit, skip, filters }
-    const rawResponse = await fetch(`${API}/products/by/search`, {
-        method: "POST",
-        headers: {
-            Accept: 'application/json',
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify(data),
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const data = { limit, skip, filters }
+        const rawResponse = await fetch(`${API}/products/by/search`, {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
 }
 
 export const list = async (params) => {
-    const query = queryString.stringify(params);
-    console.log('query', query);
-    const rawResponse = await fetch(`${API}/products/search?${query}`, {
-        method: "GET",
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const query = queryString.stringify(params);
+        console.log('query', query);
+        const rawResponse = await fetch(`${API}/products/search?${query}`, {
+            method: "GET",
+        });
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const read = async (productId) => {
-    const rawResponse = await fetch(`${API}/product/${productId}`, {
-        method: "GET",
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const rawResponse = await fetch(`${API}/product/${productId}`, {
+            method: "GET",
+        });
+
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const listRelated = async (productId) => {
-    const rawResponse = await fetch(`${API}/products/related/${productId}`, {
-        method: "GET",
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+    try {
+        const rawResponse = await fetch(`${API}/products/related/${productId}`, {
+            method: "GET",
+        });
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const getBraintreeClientToken = async (userId, token) => {
-    const rawResponse = await fetch(`${API}/braintree/getToken/${userId}`, {
-        method: "GET",
-        headers: {
-            Accept: 'application/json',
-            "Content-Type": "application/json",
-            Authorization: `bearer ${token}`,
-        },
+    try {
+        const rawResponse = await fetch(`${API}/braintree/getToken/${userId}`, {
+            method: "GET",
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json",
+                Authorization: `bearer ${token}`,
+            },
 
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+        });
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
+
 }
 
 export const processPayment = async (userId, token, paymentData) => {
-    const rawResponse = await fetch(`${API}/braintree/payment/${userId}`, {
-        method: "POST",
-        headers: {
-            Accept: 'application/json',
-            "Content-Type": "application/json",
-            Authorization: `bearer ${token}`,
-        },
-        body: JSON.stringify(paymentData),
+    try {
+        const rawResponse = await fetch(`${API}/braintree/payment/${userId}`, {
+            method: "POST",
+            headers: {
+                Accept: 'application/json',
+                "Content-Type": "application/json",
+                Authorization: `bearer ${token}`,
+            },
+            body: JSON.stringify(paymentData),
 
-    });
-    if (rawResponse.error) {
-        console.log(rawResponse.error);
-    } else {
+        });
         const response = rawResponse.json();
         return response;
+
+    } catch (error) {
+        console.log(error);
     }
 }
 
